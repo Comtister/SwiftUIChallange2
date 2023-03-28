b95444f35ee92cc80c77d7ee4a552085f4019fda
Oguzhan Ozturk <ozturk210@gmail.com>
2023-03-28 15:41:34 +0300

    Initial Commit

---
+++ SwiftUIChallange2/SwiftUIChallange2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChallange2App.swift
+//  SwiftUIChallange2
+//
+//  Created by Oguzhan Ozturk on 28.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChallange2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
